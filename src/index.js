fd570d0ee4a4c9ebecdf3b327c02d729dc7806dc
YuvrajPraveenSingh <yuv00191@outlook.com>
2024-03-20 00:44:37 +0530

    modified:   package-lock.json 	modified:   package.json 	modified:   src/app.js 	modified:   src/index.js 	new file:   src/utils/ApiError.js 	new file:   src/utils/ApiResponse.js 	new file:   src/utils/asynchandler.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import { app } from './app.js';
 import connectDB from './db/index.js';
 import dotenv from "dotenv"
 
@@ -6,4 +7,15 @@ dotenv.config({
 })
 
 
-connectDB();
+connectDB()
+.then(()=>{
+    app.on("error" , (err)=>{
+        console.log("error" , err);
+    })
+    app.listen(process.env.PORT || 8000, (err) =>{
+        console.log(`Server is Up And Running at ${process.env.PORT}`)
+    })
+})
+.catch((err)=> {
+    console.log("MongoDb Connection Fail !!!" , err)
+});
